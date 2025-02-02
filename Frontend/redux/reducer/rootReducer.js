bd4ede0ca740e8203bfb46e2a7a68321f46018ca
Bhupesh Dabas <159823863+BHUPESHDABAS@users.noreply.github.com>
2025-02-03 02:45:39 +0530

    Delete Frontend directory
    Deleting Frontend files

--- Frontend/redux/reducer/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import userReducer from './userReducer'
-import restaurantReducer from './restaurantReducer'
-
-const rootReducer = combineReducers({
-    userReducer,
-    restaurantReducer
-})
-export default rootReducer
--- Frontend/utils/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'http://localhost:4444',
-    withCredentials: true
-});
-
-
-export default instance;
